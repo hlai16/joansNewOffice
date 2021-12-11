f4bde7311edfbaff9ebf09ba35a2d4a61ff6b974
hlai16 <gloriajoanlai@gmail.com>
2021-12-10 22:53:48 -0500

    SASS installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Coming soon
-    </div>
+    <main className="App">
+      <section className="sidebar">
+        {/* <hr className="sidebar__separator sidebar--centered" /> */}
+        <nav className="sidebar__menu">
+        </nav>
+      </section>
+      <section className="displayContents">
+       
+      </section>
+    </main>
   );
 }
 
