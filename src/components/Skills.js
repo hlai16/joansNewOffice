07e608d5339e5cbb8f2a34f00b57564b9d10e5e7
hlai16 <gloriajoanlai@gmail.com>
2022-06-02 21:26:54 -0400

    adding skills to skill session

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,47 @@
-
+import './Skills.scss';
 
 function Skills() {
     return (
      <>
-      Skills display here
+      <h2>Things that I can do:</h2>
+      <h3>Frontend:</h3>
+      <p>Vanilla Javascript,
+        jQuery,
+        React,
+        HTML 5,
+        CSS 3,
+        SCSS,
+        Wordpress,
+        Bootstrap,
+        Adobe Suite,
+        Figma,
+        Wireframing/Mockup</p>
+      <h3>Backend:</h3>
+      <p>Ruby Rails,
+        Google Analytics,
+        Git,
+        node.js,
+        dotenv,
+        byebug,
+        Active Record,
+        Nodemon,
+        Express.js
+        Java</p>
+      <h3>Client Management:</h3>
+      <p>Microsoft Office,
+        Quickbooks,
+        Trello</p>
+      <h3>Database:</h3>
+      <p>SQL,
+        Firebase,
+        REST API,
+        JSON</p>
+      <h3>Testing:</h3>
+      <p>Storybook,
+        Jest,
+        Mocha/Chai,
+        Cypress,
+        RSpec</p>
      </>
     );
   }
