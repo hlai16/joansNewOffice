b581010b5d1d3f9c7c59ce12e9dbb0a596cdf445
hlai16 <gloriajoanlai@gmail.com>
2022-06-02 21:07:44 -0400

    Contents in About and some CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
-
+import './About.scss';
 
 function About() {
   return (
    <>
-    About from About.js
+    <h2>About Me</h2>
+    <p>Full Stack Web Developer with a background 
+      in teaching and instruction. I fell in love 
+      with coding and discovered this is my passion 
+      and through networking, I was able to 
+      continue learning and excited about the world 
+      of web development. I enjoy creating products 
+      that are functional and improve the user 
+      experience for all customers. I believe in 
+      sound communications. Every client is 
+      special and I like to express their 
+      uniqueness onto their website and apps.</p>
    </>
   );
 }
